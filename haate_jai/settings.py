b6c1644f1a5c4dead35a63bcc8c260dfd7c1c7d0
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-10-20 11:17:41 +0600

    host

--- haate_jai/settings.py
+++ haate_jai/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from pathlib import Path
-import os
-import dj_database_url
+# import os
+# import dj_database_url
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- profiles/views.py
+++ profiles/views.py
@@ -36,7 +36,8 @@ class UserRegistrationView(FormView):
         print("uid ", uid)
 
         # sometimes working sometimes not working the verification email link
-        confirm_link = f"http://127.0.0.1:8000/user/activate/{uid}/{token}/"
+        confirm_link = f"https://haatejai.onrender.com/user/activate/{uid}/{token}/"
+        # confirm_link = f"http://127.0.0.1:8000/user/activate/{uid}/{token}/"
         email_subject = "Confirm Your Email"
         email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
         email = EmailMultiAlternatives(email_subject , '', to=[user.email])
